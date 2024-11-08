e0dea1efe805e960ea7197d4908d0a62bbc17909
Bastien <bastien.joyeau@student.vinci.be>
2024-11-08 09:18:48 +0100

    debut tutorial routing jusqu'au bug

--- tutorials/front/medium-state/src/types.ts
+++ tutorials/front/medium-state/src/types.ts
@@ -1,7 +1,15 @@
 interface Pizza {
-    id: number;
-    title: string;
-    content: string;
-  }
-  
-  export type { Pizza };  
+  id: number;
+  title: string;
+  content: string;
+}
+
+interface Pizza {
+  id: number;
+  title: string;
+  content: string;
+}
+
+type NewPizza = Omit<Pizza, "id">;
+
+export type { Pizza, NewPizza };
---
+++ tutorials/routing/routing/src/main.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App, AboutPage, ContactPage, HomePage } from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>  
+  </React.StrictMode>
+);
