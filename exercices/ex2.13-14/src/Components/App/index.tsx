9a62755215a92cb1c130139cb05e7d737dd00a98
Bastien <bastien.joyeau@student.vinci.be>
2024-11-30 17:40:03 +0100

    ex2.14

--- exercices/ex2.13-14/src/Components/App/index.tsx
+++ exercices/ex2.13-14/src/Components/App/index.tsx
@@ -10,18 +10,18 @@ const App = () => {
   const [joke, setJoke] = useState<Joke | undefined>(undefined);
 
   useEffect(() => {
-    fetch("https://v2.jokeapi.dev/joke/Any?type=single")
+    const interval = setInterval(() => {
+
+      // Code original pour afficher une blague
+      fetch("https://v2.jokeapi.dev/joke/Any?type=single")
       .then((response) => {
-        if (!response.ok)
-          throw new Error(
-            `fetch error : ${response.status} : ${response.statusText}`
-          );
         return response.json();
       })
-      .then((joke) => setJoke({category: joke.category, joke: joke.joke}))
-      .catch((err) => {
-        console.error("HomePage::error: ", err);
-      });
+      .then((joke) => setJoke({category: joke.category, joke: joke.joke}));
+      // Fin du code original
+      
+    }, 1000);
+    return () => clearInterval(interval);
   }, []);
 
   if (!joke) return <div>Loading...</div>;
